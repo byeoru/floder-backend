edfcbed2111e1c41e019e762f6920ff5c27108c0
byeoru <byeoru215@gmail.com>
2024-01-22 17:59:01 +0900

    DB setting

--- src/main/kotlin/com/byeorustudio/plugins/Databases.kt
+++ src/main/kotlin/com/byeorustudio/plugins/Databases.kt
@@ -1,49 +1,32 @@
 package com.byeorustudio.plugins
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
-    val database = Database.connect(
-            url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-            user = "root",
-            driver = "org.h2.Driver",
-            password = ""
-        )
-    val userService = UserService(database)
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
+    val url = environment.config.property("db.url").getString()
+    val driver = environment.config.property("db.driver").getString()
+    val user = environment.config.property("db.user").getString()
+    val pw = environment.config.property("db.password").getString()
+    Database.connect(
+        url,
+        driver = driver,
+        user = user,
+        password = pw,
+    )
+
+    transaction {
+//        exec("DROP TABLE IF EXISTS users CASCADE;")
+        // Table 제거
+        SchemaUtils.drop()
+
+        // Table 생성
+//        SchemaUtils.create()
     }
 }
+
+suspend fun <T> dbQuery(block: suspend () -> T): T =
+    newSuspendedTransaction(Dispatchers.IO) { block() }
