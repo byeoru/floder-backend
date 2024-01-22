18685e13a77d6b7a40d136ecbb0083223d5d550b
byeoru <byeoru215@gmail.com>
2024-01-22 17:56:43 +0900

    Koin install

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
 val exposed_version: String by project
-val h2_version: String by project
+val koin_version: String by project
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
@@ -33,7 +32,8 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("io.ktor:ktor-server-swagger-jvm")
     implementation("io.ktor:ktor-server-openapi")
     implementation("io.ktor:ktor-server-auth-jvm")
@@ -41,7 +41,17 @@ dependencies {
     implementation("io.ktor:ktor-client-apache-jvm")
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    // log
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    // postgreSQL
+    implementation("org.postgresql:postgresql:42.6.0")
+    // https://mvnrepository.com/artifact/net.postgis/postgis-jdbc
+    implementation("net.postgis:postgis-jdbc:2023.1.0")
+    // Koin for Kotlin apps
+    implementation ("io.insert-koin:koin-ktor:$koin_version")
+    implementation ("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
--- src/main/kotlin/com/byeorustudio/Application.kt
+++ src/main/kotlin/com/byeorustudio/Application.kt
@@ -2,15 +2,17 @@ package com.byeorustudio
 
 import com.byeorustudio.plugins.*
 import io.ktor.server.application.*
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(Koin) { }
     configureSerialization()
     configureDatabases()
     configureHTTP()
-    configureSecurity()
+//    configureSecurity()
     configureRouting()
 }
